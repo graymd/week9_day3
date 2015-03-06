294ffb28708800d49fec385a331ad319c2724900
Malachi Gray <graymd.cfi@gmail.com>
2015-03-06 12:44:36 -0500

    update S3 remote access key variable

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,9 @@ class Item < ActiveRecord::Base
     "#{Rails.root}/config/aws-keys.yml"
   else
     {
-      bucket: ENV['AWS_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY'],
-      secret_access_key: ENV['AWS_SECRET_KEY']
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   end
   has_attached_file :image,
